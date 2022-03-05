4f97569d5dd0bcbceab06a11f83d3fce013e9978
Esla <suarezesla@gmail.com>
2022-03-05 11:15:51 +0100

    Ejercicios

---
+++ Ejercicios 1-2-3/script.js
@@ -0,0 +1,26 @@
+/*1.- Se trata de pedir al usuario un número a través de una ventana de entrada. Comprobar si
+es un número. El resultado mostrarlo con una ventana emergente.
+ */
+
+
+function comprobarDato() {
+
+    var resu = "";
+
+    var dato = prompt("Por favor, introduce un dato y comprobaremos si es numérico o no.");
+
+    while (isNaN(dato)) {
+
+        alert(dato + " no es un número.");
+        dato = prompt("Introduce un número.");
+
+        resu = dato;
+    }
+
+    alert(dato + " es un número.");
+
+    resu = "El número introducido es: " + dato;
+    return resu;
+}
+
+document.getElementById('ejer1').innerHTML = comprobarDato();
